7b39a7166ffe94f0ce08933870c3dda8c3f7b442
ramos <rdiego26@gmail.com>
2025-02-09 12:59:54 +0000

    chore: update gitignore

---
+++ eslint.config.cjs
@@ -0,0 +1,24 @@
+// eslint.config.js
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    ignores: ["**/webpack/**", "**/build/**", "jest.config**", "babel.config**"],
+  },
+];
